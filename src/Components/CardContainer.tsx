652cfbd7624b8950c4e94712440f387b2706cb0d
Askari <askarir64@gmail.com>
2023-10-14 00:25:02 +0530

    implemented Search Functionality

--- src/Components/CardContainer.tsx
+++ src/Components/CardContainer.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const CardContainer = (props: { symbol: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined }) => {
   return (
     <div>
-        <p>{props.symbol}</p>
-        <p>{props.title}</p>
+        <h2>{props.symbol}</h2>
+        <h2>{props.title}</h2>
     </div>
   )
 }
--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,19 +1,46 @@
-import { ChangeEvent, useState } from "react"
-import emojiList from '../emojisList.json'
+import { ChangeEvent, useState } from "react";
+import emojiList from "../emojisList.json";
+import CardContainer from "./CardContainer";
 
 function Search() {
-    const [input, setInput] = useState("")
+  const [input, setInput] = useState("");
+  const [searchResult, setSearchResult] = useState<{ title: string; symbol: string }[]>([]);
 
-    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
-        setInput(e.target.value)
-    }
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const searchFunc = () => {
+    let searchedItems = emojiList.filter((emoji) => {
+      return emoji.keywords.includes(input);
+    });
+    setSearchResult(searchedItems);
+  };
+  
 
   return (
-    <div className="flex justify-center items-center font-serif font-normal tracking-normal">
-        <input className='rounded border-black mt-4' type="search" placeholder="smiley face" value={input} onChange={handleChange} />
-        <button>Search</button>
+    <div className="flex justify-center flex-col items-center font-serif font-normal tracking-normal">
+      <div>
+        <input
+          className="rounded border-black mt-4"
+          type="search"
+          placeholder="smiley face"
+          value={input}
+          onChange={handleChange}
+        />
+        <button onClick={searchFunc}>Search</button>
+      </div>
+      <div>
+        {searchResult.map((emoji) => (
+          <CardContainer
+            key={emoji.title}
+            title={emoji.title}
+            symbol={emoji.symbol}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
